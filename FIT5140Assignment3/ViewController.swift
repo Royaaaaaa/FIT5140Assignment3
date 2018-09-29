208cb0a1a145cae4116a5a825fff8431a8404404
妈妈 <mac@dyn-118-139-36-227.its.monash.edu.au>
2018-09-29 13:09:58 +1000

    Initial Commit

---
+++ FIT5140Assignment3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FIT5140Assignment3
+//
+//  Created by 隋晓婷 on 2018/9/29.
+//  Copyright © 2018 Maya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
