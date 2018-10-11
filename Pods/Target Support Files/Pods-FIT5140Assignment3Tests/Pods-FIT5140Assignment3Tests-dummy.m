93f74785a58c3e7fb296536affdad1423f319e85
Royaaaaaa <43308173+Royaaaaaa@users.noreply.github.com>
2018-10-11 20:40:34 +1100

    assign

---
+++ Pods/Target Support Files/Pods-FIT5140Assignment3Tests/Pods-FIT5140Assignment3Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FIT5140Assignment3Tests : NSObject
+@end
+@implementation PodsDummy_Pods_FIT5140Assignment3Tests
+@end
---
+++ Pods/Target Support Files/Pods-FIT5140Assignment3UITests/Pods-FIT5140Assignment3UITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FIT5140Assignment3UITests : NSObject
+@end
+@implementation PodsDummy_Pods_FIT5140Assignment3UITests
+@end
