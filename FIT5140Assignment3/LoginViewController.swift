f47962468c18d00ab858bf1b27fe3c9aeb3f1e52
GaoYuer <gaoyu620123@me.com>
2018-10-14 12:27:25 +1100

    Login

---
+++ FIT5140Assignment3/LoginViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LoginViewController.swift
+//  FIT5140Assignment3
+//
+//  Created by YU MUSE on 14/10/18.
+//  Copyright © 2018 Maya. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBAction func loginButton(_ sender: Any) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+
+}
