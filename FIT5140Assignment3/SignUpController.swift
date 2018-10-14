010d035b997e1fcb7ec0e848909d21b5cf2f85a0
Royaaaaaa <43308173+Royaaaaaa@users.noreply.github.com>
2018-10-14 12:27:19 +1100

    signUp

---
+++ FIT5140Assignment3/SignUpController.swift
@@ -0,0 +1,35 @@
+//
+//  SignUpController.swift
+//  FIT5140Assignment3
+//
+//  Created by 隋晓婷 on 2018/10/14.
+//  Copyright © 2018 Maya. All rights reserved.
+//
+
+import UIKit
+
+class SignUpController: UIViewController {
+
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var confirmPasswordTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func signUpButton(_ sender: Any) {
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
